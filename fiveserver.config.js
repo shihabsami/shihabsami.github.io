c0b544debebb49bd1e4bfbcb45294731f5a94dd7
Shihab Sami <shihabsami14120@gmail.com>
2022-06-27 19:06:48 +1000

    Implement navigation arrow

--- fiveserver.config.js
+++ fiveserver.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: 8080,
   open: ["./public/index.html"],
-  watch: ["./public/index.html", "./public/style.css"],
+  watch: ["./public/index.html", "./public/style.css", "./src/index.js"],
   host: "0.0.0.0",
   browser: "chrome --incognito",
 };
--- src/index.js
+++ src/index.js
@@ -7,4 +7,64 @@ window.onload = function () {
   }
 
   themeSwitch.addEventListener("change", switchTheme, false);
+
+  function handleScroll() {
+    let elements = document.getElementsByClassName("page-item");
+    const arrowTextElement = document.getElementById("arrow-text");
+    const topArrowButton = document.getElementById("top-arrow-button");
+    const bottomArrowButton = document.getElementById("bottom-arrow-button");
+
+    let scrollPosition =
+      window.pageYOffset || window.scrollY || document.body.scrollTop || document.documentElement.scrollTop;
+
+    let closestElementIndex = 0;
+    let closestDistance = Infinity;
+    for (let i = 0; i < elements.length; i++) {
+      let element = elements[i];
+      let distance = Math.abs(scrollPosition - element.offsetTop);
+      if (distance <= closestDistance) {
+        closestElementIndex = i;
+        closestDistance = distance;
+      }
+    }
+
+    arrowTextElement.innerHTML = elements[closestElementIndex].id
+      .at(0)
+      .toUpperCase()
+      .concat(elements[closestElementIndex].id.substring(1));
+
+    arrowTextElement.classList.forEach((className) => arrowTextElement.classList.remove(className));
+    arrowTextElement.classList.add("row-span-1", "text-md", "self-center", "rotate-90", "transition-all", "bg-white");
+    switch (closestElementIndex) {
+      case 0:
+        arrowTextElement.classList.add("row-start-1");
+        topArrowButton.classList.add("hidden");
+        bottomArrowButton.classList.remove("hidden");
+        break;
+      case 1:
+        arrowTextElement.classList.add("row-start-2");
+        topArrowButton.classList.remove("hidden");
+        bottomArrowButton.classList.remove("hidden");
+        break;
+      case 2:
+        arrowTextElement.classList.add("row-start-3");
+        topArrowButton.classList.remove("hidden");
+        bottomArrowButton.classList.remove("hidden");
+        break;
+      case 3:
+        arrowTextElement.classList.add("row-start-4");
+        topArrowButton.classList.remove("hidden");
+        bottomArrowButton.classList.remove("hidden");
+        break;
+      case 4:
+        arrowTextElement.classList.add("row-start-5");
+        topArrowButton.classList.remove("hidden");
+        bottomArrowButton.classList.add("hidden");
+        break;
+      default:
+        break;
+    }
+  }
+
+  window.addEventListener("scroll", handleScroll);
 };
