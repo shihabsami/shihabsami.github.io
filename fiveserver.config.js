a8b5e85d73f61796df3423077e74782b0089bc20
Shihab Sami <shihabsami14120@gmail.com>
2022-08-11 01:19:42 +1000

    Flatten directory structure

--- fiveserver.config.js
+++ fiveserver.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: 8080,
-  open: ["./public/index.html"],
-  watch: ["./public/index.html", "./public/style.css", "./src/index.js"],
+  open: ["index.html"],
+  watch: ["index.html", "style.css", "index.js"],
   host: "0.0.0.0",
   browser: "chrome",
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html"],
+  content: ["index.html"],
   darkMode: "class",
   theme: {
     extend: {},
